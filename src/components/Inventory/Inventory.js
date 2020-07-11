9facb716fac92ce7298c545d6c4913d8a5b86a1d
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-11 21:38:15 +0600

    Shop.js - fakeData removed - mongoDB data added

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import fakeData from '../../fakeData';
 
 const Inventory = () => {
+    const handleAddInventory = () => {
+        const product = fakeData[0]
+        console.log('before post', fakeData[0])
+        fetch('http://localhost:3005/addProduct', {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(fakeData) // body data type must match "Content-Type" header
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('post success', data)
+            })
+
+
+    }
     return (
         <div>
-            <h2>This is inventory</h2>
+            <h2>Add more product to sell...</h2>
+            <button onClick={handleAddInventory}>Add Invetory</button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import fakeData from "../../fakeData";
 import "./Shop.css";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
@@ -7,27 +6,36 @@ import { addToDatabaseCart, getDatabaseCart } from "../../utilities/databaseMana
 import { Link } from "react-router-dom";
 
 const Shop = () => {
-  const firstTen = fakeData.slice(0, 10);
-  const [products, setProducts] = useState(firstTen);
+  const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
+    fetch('http://localhost:3005/products')
+      // node-mongo-2 local server - provides data from mongoDB
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data);
+      })
+  }, [])
+  useEffect(() => {
     //cart
     const savedCart = getDatabaseCart();
     const productKeys = Object.keys(savedCart);
-    const cartProducts =  productKeys.map(key=>{
-        const product = fakeData.find(pd=>pd.key===key);
+    if (products.length) {
+      const cartProducts = productKeys.map(key => {
+        const product = products.find(pd => pd.key === key);
         product.quantity = savedCart[key];
         return product;
-    });
-    setCart(cartProducts);
-},[])
+      });
+      setCart(cartProducts);
+    }
+  }, [products])
   const handleProduct = (product) => {
     const toBeAddedKey = product.key;
-    const sameProduct = cart.find(pd=>pd.key===toBeAddedKey);
+    const sameProduct = cart.find(pd => pd.key === toBeAddedKey);
     let count = 1;
     let newCart;
     if (sameProduct) {
-     count = sameProduct.quantity + 1;
+      count = sameProduct.quantity + 1;
       sameProduct.quantity = count;
       const others = cart.filter((pd) => pd.key !== toBeAddedKey);
       newCart = [...others, sameProduct];
